f1350cb3acd8c12cefae6860f1f1584390f00b50
rhishikesh-chikhalkar <rhishichikhalkar21@gmail.com>
2024-05-05 23:09:46 +0530

    added code to auto update env vars

--- cmd/cmd_auto_setting_env_variables.cmd
+++ cmd/cmd_auto_setting_env_variables.cmd
@@ -1,1 +1,19 @@
 @echo off
+setlocal enabledelayedexpansion
+
+:: Specify the path to your .env file
+set ENV_FILE_PATH=E:\Softwares\setting_env_vars\.env
+
+:: Read each line from the .env file
+for /f "delims=" %%a in ('type "%ENV_FILE_PATH%"') do (
+    :: Split the line into KEY and VALUE
+    set "line=%%a"
+    for /f "tokens=1* delims==" %%b in ("!line!") do (
+        set KEY=%%b
+        set VALUE=%%c
+        :: Set the environment variable using setx for system-wide, replace with set for current session
+        setx !KEY! !VALUE!
+    )
+)
+
+echo Environment variables have been set.
